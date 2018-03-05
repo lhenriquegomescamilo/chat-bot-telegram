42e997b2d1c65edbf81cd5a0478143aebd2173c4
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2018-03-04 21:01:33 -0300

    master: Configurando o chat bot

---
+++ src/main/java/com/chatbot/example/ChatBot.java
@@ -0,0 +1,52 @@
+package com.chatbot.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ * This example bot is an echo bot that just repeats the messages sent to him
+ */
+@Component
+public class ChatBot extends TelegramLongPollingBot {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatBot.class);
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            final String text = update.getMessage().getText();
+            final Long chatId = update.getMessage().getChatId();
+            SendMessage message = new SendMessage().setChatId(chatId).setText(text);
+
+            try {
+                super.sendMessage(message);
+                logger.info("Sent message \"{}\" to {}", text, chatId);
+            } catch (TelegramApiException e) {
+                logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+            }
+        }
+    }
+
+}
